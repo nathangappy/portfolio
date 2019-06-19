105bf5c3740cb73bdfb253afb6864ee0c97698a6
g4pm0n3y <nate.gappy@gmail.com>
2019-06-19 11:39:35 -0400

    add responsive slide menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSideMenu() {
+  document.getElementById('side-menu').style.width = '50%';
+  // document.getElementById('side-menu').style.display = 'flex';
+}
+function closeSideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+  // document.getElementById('side-menu').style.display = 'none';
+}
